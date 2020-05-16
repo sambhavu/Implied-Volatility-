5f045554851be4eac2da8e1ca1759f86b6b293fc
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-05-16 16:31:14 +0530

    Create iv_finder.cpp

---
+++ iv_finder.cpp
@@ -0,0 +1,219 @@
+import<iostream>
+import<algorithm> 
+import<cmath> 
+using namespace std; 
+
+#ifndef Pi 
+#define Pi 3.141592653589793238462643 
+#endif 
+
+
+class option{
+public:
+     
+     double Und, stike, rf, sig_guess, t, CP; //CP call price  
+     int type; /0 for call, 1 for put 
+
+     int it=0; //iterations 
+     
+     option(); 
+
+     double call_price(double S, double K, double r, double sigma, double time);
+     double put_price(double S, double K, double r, double sigma, double time); 
+     double cnd(double x);
+
+     void search(); 
+     double call_search(); 
+     double put_search(); 
+
+     void display_parameters(); 
+
+}; 
+
+option::option()
+{
+     CP = 2.34; 
+     und=100;
+     stike=105;
+     rf=.04;
+     sig_guess=.01;
+     t=.5;
+
+     type=0; 
+}
+
+double option::call_search()
+{
+    double sig_est = sig_guess; 
+    double price; 
+    double ERROR =.0000001; 
+    
+    do{ 
+
+       price=call_price(und,strike,rf,sig_est,t); 
+
+      if(price<CP-ERROR) 
+      {
+         sig_est+=ERROR; 
+      } 
+
+      else{
+         sig_est-=ERROR; 
+      } 
+
+       it++; 
+
+    }while(price<CP-ERROR || price>CP+Error) 
+
+    return sig_est; 
+
+
+}
+
+
+
+double option::put_search()
+{
+    double sig_est = sig_guess; 
+    double price; 
+    double ERROR =.0000001; 
+    
+    do{ 
+
+       price=put_price(und,strike,rf,sig_est,t); 
+
+      if(price<CP-ERROR) 
+      {
+         sig_est+=ERROR; 
+      } 
+
+      else{
+         sig_est-=ERROR; 
+      } 
+
+       it++; 
+
+    }while(price<CP-ERROR || price>CP+Error) 
+
+    return sig_est; 
+
+
+}
+
+
+
+option:: search()
+{
+
+   if(type==1)
+   {
+     call_search(); 
+   } 
+   else{
+     put_search(); 
+   } 
+}
+
+void option:: display_parameters()
+{
+  if(int == 0) 
+   {
+     cout<<"Call\n"; 
+   } 
+   else { 
+     cout<<"Put\n"; 
+    } 
+
+    cout<<"\nUnderlying Price    -> "<<und;
+    cout<<"\nStike Price         -> "<<strike;
+    cout<<"\nTime to Exp         -> "<<t<<" years";
+    cout<<"\nRisk free rate      -> "<<rf*100<<"%";
+    cout<<"\n";
+    cout<<"\nGuess Volatility    -> "<<sig_guess*100<<"%"; 
+
+
+}
+
+double option :: call_price(
+                            double S,
+                            double K,
+                            double r,
+                            double sigma,
+                            double time)
+{
+        
+        double time_sqrt = sqrt(time);
+        
+        
+       /* double d1 = (log(S/K)+r*time)/(sigma*time_sqrt)+.5*sigma*time_sqrt; */
+        
+        double d1 = (log(S/K) + (r+sigma*sigma*.5)*time)/(sigma*time_sqrt);
+       
+       
+       
+        double d2 = d1-(sigma*time_sqrt);
+        
+        double price = S*cnd(d1)-K*exp(-r*time)*cnd(d2);
+        
+        return price; 
+}
+ 
+
+
+double option :: put_price(
+                            double S,
+                            double K,
+                            double r,
+                            double sigma,
+                            double time)
+{
+        
+        double time_sqrt = sqrt(time);
+        
+        
+       /* double d1 = (log(S/K)+r*time)/(sigma*time_sqrt)+.5*sigma*time_sqrt; */
+        
+        double d1 = (log(S/K) + (r+sigma*sigma*.5)*time)/(sigma*time_sqrt);
+       
+        double d2 = d1-(sigma*time_sqrt);
+        
+        double price = -S*cnd(-d1)+K*exp(-r*time)*cnd(-d2);
+        
+        return price; 
+    }
+    
+
+    
+double option:: cnd(double x)
+{
+  double L, K, w ;
+  /* constants */
+  double const a1 = 0.31938153, a2 = -0.356563782, a3 = 1.781477937;
+  double const a4 = -1.821255978, a5 = 1.330274429;
+
+  L = fabs(x);
+  K = 1.0 / (1.0 + 0.2316419 * L);
+  w = 1.0 - 1.0 / sqrt(2 * Pi) * exp(-L *L / 2) * (a1 * K + a2 * K *K + a3 * pow(K,3) + a4 * pow(K,4) + a5 * pow(K,5));
+
+  if (x < 0 ){
+    w= 1.0 - w;
+  }
+  return w;
+}
+
+
+int main(); 
+{
+
+option iv_finder;
+
+iv_finder.display_parameters();
+iv_finder.search(); 
+
+
+
+
+return 0; 
+
+
+}
