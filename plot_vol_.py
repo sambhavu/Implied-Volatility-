f6380308e2f3eb914cd17b6fab1b323dd9b81b8a
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-08-13 17:41:57 -0400

    Create plot_vol_.py

---
+++ plot_vol_.py
@@ -0,0 +1,55 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.tri as mtri
+
+
+a = []
+b = []
+c = []
+
+apple = yf.Ticker('AAPL')
+apple_exp_dates = apple.options
+
+for date in apple_exp_dates:
+    chain = apple.option_chain(date)
+    iv = chain.calls.impliedVolatility
+    strikes = chain.calls.strike
+
+    len_  = len(strikes)
+
+    expiry = []
+    strike_price = []
+    impliedvol = []
+
+    for i in range(0,len_):
+        expiry.append(date)
+        impliedvol.append(iv[i])
+        strike_price.append(strikes[i])
+
+    expiry = np.asarray(expiry)
+    impliedvol = np.asarray(impliedvol)
+    strike_price = np.asarray(strike_price)
+
+    plt.plot(impliedvol)
+    a.append(expiry)
+    b.append(impliedvol)
+    c.append(strike_price)
+
+
+a = np.asarray(a)
+b = np.asarray(b)
+c = np.asarray(c)
+
+plt.show()
+
+
+#s, t = np.meshgrid(a,c)
+#fig = plt.figure()
+##volsurface = fig.add_subplot(111, projection = '3d')
+#volsurface.scatter(a,b,c)
+
+#plt.show()
