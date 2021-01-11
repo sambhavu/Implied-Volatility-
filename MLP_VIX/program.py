6fa569ea61c1d82a9655ba60fe36606b54ea791f
simpleassociate <30390294+simpleassociate@users.noreply.github.com>
2021-01-10 22:56:35 -0500

    Update program.py

--- MLP_VIX/program.py
+++ MLP_VIX/program.py
@@ -3,9 +3,11 @@ from yahoofinancials import YahooFinancials
 import numexpr as ne
 import pandas as pd
 import numpy as np
+import scipy
 from matplotlib import pyplot as plt
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
+
 
 
 dataset = pd.read_csv("/users/satish/Desktop/dataset.txt", names = ["dat"])
